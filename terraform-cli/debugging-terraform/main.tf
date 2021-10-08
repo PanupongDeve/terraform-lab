e15bb17bf89e543fbb5b772bb503afdcbf600f10
Panupong Chamsomboon <5635512110@psu.ac.th>
2021-10-08 01:54:44 +0000

    add bugging terraform lab code

---
+++ terraform-cli/debugging-terraform/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "2.15.0"
+    }
+  }
+}
+
+
+# Configure the Docker provider
+provider "docker" {}
+
+#Image to be used by container
+resource "docker_image" "terraform-centos" {
+  name         = "centos:7"
+  keep_locally = true
+}
+
+# Create a container
+resource "docker_container" "centos" {
+  image   = docker_image.terraform-centos.latest
+  name    = "terraform-centos"
+  start   = true
+  command = ["/bin/sleep", "500"]
+}
